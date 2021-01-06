3065bf641b3e9a5cb4ab9b26ea7d13614fba3963
asmaa22-tech <you@example.com>
2021-01-06 18:25:54 +0200

    feed update 0.1

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,6 @@ import 'package:Paws/AdoptScreen/adopt.dart';
 import 'package:Paws/ShelterScreen/shelters.dart';
 <<<<<<< HEAD
 =======
-import 'package:Paws/StrayScreen/stray.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 >>>>>>> 7512b8fd7ab941320c6f70e52f88f40d1d1ed4b9
 
